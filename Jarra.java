e1db26a738e9fba9fb4330cc69484bd032d7a68f
Alexandra Rodríguez Serantes <you@example.com>
2024-02-29 20:07:44 +0100

    Solucion error

--- Jarra.java
+++ Jarra.java
@@ -1,26 +1,49 @@
-public void llena() {
-    this.cantidad = this.capacidad;
-}
+public class Jarra { //atributos
+	private int cantidad;
+	private int capacidad;
+	//constructor
+	public Jarra(int capacidadInicial){
+		if(capacidadInicial<=0){
+			throw new RuntimeException("ERROR: no se pueden crear jarras
+						con capacidad menor o igual que cero.");
+ 		}
+ 		this.capacidad=capacidadInicial;
+ 	}
+ 	// metodos
+ 	// 1
+ 	// 2
+ 	// 3
 
-public void vacia() {
-    this.cantidad = 0;
-}
+	public void llena() {
+		this.cantidad = this.capacidad;
+	}	
+        
+	public void vacia() {
+		    this.cantidad = 0;
+	}
+	
+	public void llenaDesde(Jarra j) {
+		while ((this.cantidad < this.capacidad) && (j.cantidad > 0)){
+			this.cantidad++;
+			j.cantidad--;
+		}
+	}
 
-public void llenaDesde(Jarra j) {
-    while ((this.cantidad < this.capacidad) && (j.cantidad > 0)) {
-        this.cantidad++;
-        j.cantidad--;
-    }
-}
+	public String toString() {
+		return "(" + this.capacidad + "," + this.cantidad + ")";
+	}
 
-public String toString() {
-    return "(" + this.capacidad + "," + this.cantidad + ")";
-}
+	public int capacidad() {
+		return this.capacidad;
+	}
 
-public int capacidad() {
-    return this.capacidad;
-}
+	public int cantidad() {
+		return this.cantidad;
+	}
 
-public int cantidad() {
-    return this.cantidad;
+ 	public void metodoComunitario(){
+ 		// Comentario incluido para pruebas
+		System.out.println("Aqui escribimos todos");
+ 	}
+
 }
